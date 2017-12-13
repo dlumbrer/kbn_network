c46f9dd56fa520dff0f5a468b9c35b1e7e268979
David Moreno Lumbreras <dmorenolumb@gmail.com>
2017-12-13 10:36:01 +0100

    Support to Kibana 6

--- public/network_vis.js
+++ public/network_vis.js
@@ -1,10 +1,18 @@
 import "plugins/network_vis/network_vis.less";
 import 'plugins/network_vis/network_vis_controller';
-import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+
+import 'ui/agg_table';
+import 'ui/agg_table/agg_table_group';
+
+import { CATEGORY } from 'ui/vis/vis_category';
+import { VisFactoryProvider } from 'ui/vis/vis_factory';
+import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
-import { VisSchemasProvider } from 'ui/vis/schemas';
+import image from './images/icon-network.svg';
 import networkVisTemplate from 'plugins/network_vis/network_vis.html';
 import networkVisParamsTemplate from 'plugins/network_vis/network_vis_params.html';
+
+
 
 
 // register the provider with the visTypes registry
@@ -12,17 +20,19 @@ VisTypesRegistryProvider.register(NetworkVisTypeProvider);
 
 // define the TableVisType
 function NetworkVisTypeProvider(Private) {
-  const TemplateVisType = Private(TemplateVisTypeProvider);
+  const VisFactory = Private(VisFactoryProvider);
+
   const Schemas = Private(VisSchemasProvider);
   // return the visType object, which kibana will use to display and configure new
   // Vis object of this type.
-  return new TemplateVisType({
+  return VisFactory.createAngularVisualization({
     name: 'network',
     title: 'Network',
-    icon: 'fa-cogs',
+    image,
     description: 'Displays a network node that link two fields that have been selected.',
-    template: networkVisTemplate,
-    params: {
+    category: CATEGORY.OTHER,
+
+    visConfig: {
       defaults: {
         showLabels: true,
         showPopup: false,
@@ -49,7 +59,56 @@ function NetworkVisTypeProvider(Private) {
         gravitationalConstant: -35000,
         labelColor: '#000000'
       },
-      editor: networkVisParamsTemplate
+      template: networkVisTemplate,
+    },
+    editorConfig: {
+      optionsTemplate: networkVisParamsTemplate,
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'size_node',
+          title: 'Node Size',
+          max: 1
+          /*
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+          */
+          //aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
+        },
+        {
+          group: 'metrics',
+          name: 'size_edge',
+          title: 'Edge Size',
+          max: 1,
+        },
+        {
+          group: 'buckets',
+          name: 'first',
+          icon: 'fa fa-circle-thin',
+          mustBeFirst: 'true',
+          title: 'Node',
+          min: 1,
+          max: 2,
+          aggFilter: ['terms']//Only have sense choose terms
+        },
+        {
+          group: 'buckets',
+          name: 'second',
+          icon: 'fa fa-random',
+          title: 'Relation',
+          max: 1,
+          aggFilter: ['terms']
+        },
+        {
+          group: 'buckets',
+          name: 'colornode',
+          icon: 'fa fa-paint-brush',
+          title: 'Node Color',
+          max: 1,
+          aggFilter: ['terms']
+        }
+      ])
     },
 
     ////////MIRAR THIS
@@ -58,52 +117,7 @@ function NetworkVisTypeProvider(Private) {
     },
     ////////////////////
 
-    schemas: new Schemas([
-      {
-        group: 'metrics',
-        name: 'size_node',
-        title: 'Node Size',
-        max: 1
-        /*
-        defaults: [
-          { type: 'count', schema: 'metric' }
-        ]
-        */
-        //aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
-      },
-      {
-        group: 'metrics',
-        name: 'size_edge',
-        title: 'Edge Size',
-        max: 1,
-      },
-      {
-        group: 'buckets',
-        name: 'first',
-        icon: 'fa fa-circle-thin',
-        mustBeFirst: 'true',
-        title: 'Node',
-        min: 1,
-        max: 2,
-        aggFilter: ['terms']//Only have sense choose terms
-      },
-      {
-        group: 'buckets',
-        name: 'second',
-        icon: 'fa fa-random',
-        title: 'Relation',
-        max: 1,
-        aggFilter: ['terms']
-      },
-      {
-        group: 'buckets',
-        name: 'colornode',
-        icon: 'fa fa-paint-brush',
-        title: 'Node Color',
-        max: 1,
-        aggFilter: ['terms']
-      }
-    ])
+
   });
 }
 
